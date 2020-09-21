12c74d91cc3f5be0a677ddbca30494dbcc9d180a
stfcrux <jinweiloh1998@gmail.com>
2020-09-21 16:40:47 +0800

    update
    added user.js and mad euse of flashmessenger

---
+++ routes/main.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const router = express.Router();
+const alertMessage = require('../helpers/messenger');
+
+
+router.get('/', (req, res) => {
+	const title = 'Video Jotter';
+	res.render('index', {title: title}) // renders views/index.handlebars
+});
+
+
+router.get('/showLogin', (req, res) => {
+	res.render('user/login') // renders views/users/login.handlebars
+});
+
+router.get('/showRegister', (req, res) => {
+	res.render('user/register') // renders views/users/register.handlebars
+});
+
+router.get('/about', (req, res) => {
+	const author = 'Jin wei';
+	const version = '1.0.1'; 
+
+	alertMessage(res,'success','This is an important message','fas fa-sign-in-alt',true);
+	alertMessage(res, 'danger', 'Unauthorised access', 'fas fa-exclamation-circle',false);
+
+	let success_msg = "Success Msg"
+	let error_msg = "error Message using error_msg"
+	
+	
+	let errors = [
+					{text: "first error msg"}, // does not have to be called text, its just the key
+					{text: "Second error msg"},
+					{text: "Third error msg"}
+				];
+
+	res.render('about', {
+		author:author,
+		version:version,
+		// success_msg:success_msg,
+		// error_msg:error_msg,
+		// errors: errors
+	});
+});
+
+
+// Logout User
+router.get('/logout', (req, res) => {
+	req.logout();
+	res.redirect('/');
+});
+
+module.exports = router;
